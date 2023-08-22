74b7d103b4c139003cb22f21416f7d9333ef5642
Alexander Winter <a.w1nter@hotmail.com>
2023-08-22 19:12:25 -0400

    Code cleanup

--- src/main/java/com/github/winteralexander/gdx/utils/io/SerializationUtil.java
+++ src/main/java/com/github/winteralexander/gdx/utils/io/SerializationUtil.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.utils.ObjectMap;
 import java.io.*;
 
 import static com.badlogic.gdx.graphics.Color.rgba8888;
+import static com.github.winteralexander.gdx.utils.io.StreamUtil.*;
 
 /**
  * Serialization utility class
@@ -27,7 +28,8 @@ public class SerializationUtil {
 	private static final byte BYTE_T = 5;
 	private static final byte NULL_T = 6;
 
-	public static <T extends Readable> T readSerializable(InputStream inputStream, Class<T> type) throws IOException {
+	public static <T extends Readable> T readSerializable(InputStream inputStream,
+	                                                      Class<T> type) throws IOException {
 		T newInstance;
 		try {
 			newInstance = type.getDeclaredConstructor().newInstance();
@@ -39,70 +41,85 @@ public class SerializationUtil {
 		return newInstance;
 	}
 
-	public static <T> void readSmallArray(InputStream stream, Class<T> type, Array<T> out) throws IOException {
+	public static <T> void readSmallArray(InputStream stream,
+	                                      Class<T> type,
+	                                      Array<T> out) throws IOException {
 		out.clear();
-		int size = StreamUtil.readShort(stream);
+		int size = readShort(stream);
 		out.ensureCapacity(size);
 		while(size-- > 0)
 			out.add(readAny(stream, type));
 	}
 
-	public static void writeSmallArray(OutputStream stream, Array<?> array) throws IOException {
-		StreamUtil.writeShort(stream, array.size);
+	public static void writeSmallArray(OutputStream stream,
+	                                   Array<?> array) throws IOException {
+		writeShort(stream, array.size);
 		for(int i = 0; i < array.size; i++)
 			writeAny(stream, array.get(i));
 	}
 
-	public static <T> void readArray(InputStream stream, Class<T> type, Array<T> out) throws IOException {
+	public static <T> void readArray(InputStream stream,
+	                                 Class<T> type,
+	                                 Array<T> out) throws IOException {
 		out.clear();
-		int size = StreamUtil.readInt(stream);
+		int size = readInt(stream);
 		out.ensureCapacity(size);
 		while(size-- > 0)
 			out.add(readAny(stream, type));
 	}
 
-	public static void writeArray(OutputStream stream, Array<?> array) throws IOException {
-		StreamUtil.writeInt(stream, array.size);
+	public static void writeArray(OutputStream stream,
+	                              Array<?> array) throws IOException {
+		writeInt(stream, array.size);
 		for(int i = 0; i < array.size; i++)
 			writeAny(stream, array.get(i));
 	}
 
-	public static <K, V> void readMap(InputStream stream, Class<K> keyType, Class<V> valueType, ObjectMap<K, V> out) throws IOException {
+	public static <K, V> void readMap(InputStream stream,
+	                                  Class<K> keyType,
+	                                  Class<V> valueType,
+	                                  ObjectMap<K, V> out) throws IOException {
 		out.clear();
-		int size = StreamUtil.readInt(stream);
+		int size = readInt(stream);
 		out.ensureCapacity(size);
 		while(size-- > 0)
 			out.put(readAny(stream, keyType), readAny(stream, valueType));
 	}
 
-	public static void writeMap(OutputStream stream, ObjectMap<?, ?> map) throws IOException {
-		StreamUtil.writeInt(stream, map.size);
+	public static void writeMap(OutputStream stream,
+	                            ObjectMap<?, ?> map) throws IOException {
+		writeInt(stream, map.size);
 		for(ObjectMap.Entry<?, ?> entry : map.entries()) {
 			writeAny(stream, entry.key);
 			writeAny(stream, entry.value);
 		}
 	}
 
-	public static void readColor(InputStream stream, Color out) throws IOException {
-		Color.argb8888ToColor(out, StreamUtil.readInt(stream));
+	public static void readColor(InputStream stream,
+	                             Color out) throws IOException {
+		Color.argb8888ToColor(out, readInt(stream));
 	}
 
-	public static void writeColor(OutputStream stream, Color color) throws IOException {
-		StreamUtil.writeInt(stream, Color.argb8888(color));
+	public static void writeColor(OutputStream stream,
+	                              Color color) throws IOException {
+		writeInt(stream, Color.argb8888(color));
 	}
 
-	public static void readRGB(InputStream stream, Color out) throws IOException {
+	public static void readRGB(InputStream stream,
+	                           Color out) throws IOException {
 		float prevA = out.a;
-		Color.argb8888ToColor(out, StreamUtil.readInt24(stream));
+		Color.argb8888ToColor(out, readInt24(stream));
 		out.a = prevA;
 	}
 
-	public static void writeRGB(OutputStream stream, Color color) throws IOException {
-		StreamUtil.writeInt24(stream, Color.argb8888(color));
+	public static void writeRGB(OutputStream stream,
+	                            Color color) throws IOException {
+		writeInt24(stream, Color.argb8888(color));
 	}
 
-	public static void readBuffered(InputStream stream, Serializable serializable) throws IOException {
-		int size = StreamUtil.readInt(stream);
+	public static void readBuffered(InputStream stream,
+	                                Readable serializable) throws IOException {
+		int size = readInt(stream);
 
 		if(size < 0)
 			throw new IOException("Invalid size of buffer: " + size);
@@ -123,66 +140,69 @@ public class SerializationUtil {
 		serializable.readFrom(new ByteArrayInputStream(data));
 	}
 
-	public static void writeBuffered(OutputStream stream, Serializable serializable) throws IOException {
+	public static void writeBuffered(OutputStream stream,
+	                                 Writable serializable) throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		serializable.writeTo(baos);
-		StreamUtil.writeInt(stream, baos.size());
+		writeInt(stream, baos.size());
 		baos.writeTo(stream);
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T> T readPrimitive(InputStream stream, Class<T> type) throws IOException {
+	public static <T> T readPrimitive(InputStream stream,
+	                                  Class<T> type) throws IOException {
 		if(type == int.class || type == Integer.class)
-			return (T)Integer.valueOf(StreamUtil.readInt(stream));
+			return (T)Integer.valueOf(readInt(stream));
 
 		if(type == float.class || type == Float.class)
-			return (T)Float.valueOf(StreamUtil.readFloat(stream));
+			return (T)Float.valueOf(readFloat(stream));
 
 		if(type == long.class || type == Long.class)
-			return (T)Long.valueOf(StreamUtil.readLong(stream));
+			return (T)Long.valueOf(readLong(stream));
 
 		if(type == short.class || type == Short.class)
-			return (T)Short.valueOf(StreamUtil.readShort(stream));
+			return (T)Short.valueOf(readShort(stream));
 
 		if(type == byte.class || type == Byte.class)
-			return (T)Byte.valueOf(StreamUtil.readByte(stream));
+			return (T)Byte.valueOf(readByte(stream));
 
 		if(type == double.class || type == Double.class)
-			return (T)Double.valueOf(StreamUtil.readDouble(stream));
+			return (T)Double.valueOf(readDouble(stream));
 
 		if(type == char.class || type == Character.class)
-			return (T)Character.valueOf(StreamUtil.readChar(stream));
+			return (T)Character.valueOf(readChar(stream));
 
 		if(type == boolean.class || type == Boolean.class)
-			return (T)Boolean.valueOf(StreamUtil.readBoolean(stream));
+			return (T)Boolean.valueOf(readBoolean(stream));
 
 		throw new IllegalArgumentException("Specified type must be primitive");
 	}
 
-	public static void writePrimitive(OutputStream outputStream, Object primitive) throws IOException {
+	public static void writePrimitive(OutputStream outputStream,
+	                                  Object primitive) throws IOException {
 		if(primitive instanceof Integer)
-			StreamUtil.writeInt(outputStream, (Integer)primitive);
+			writeInt(outputStream, (Integer)primitive);
 
 		else if(primitive instanceof Float)
-			StreamUtil.writeFloat(outputStream, (Float)primitive);
+			writeFloat(outputStream, (Float)primitive);
 
 		else if(primitive instanceof Long)
-			StreamUtil.writeLong(outputStream, (Long)primitive);
+			writeLong(outputStream, (Long)primitive);
 
 		else if(primitive instanceof Short)
-			StreamUtil.writeShort(outputStream, (Short)primitive);
+			writeShort(outputStream, (Short)primitive);
 
 		else if(primitive instanceof Byte)
-			StreamUtil.writeByte(outputStream, (Byte)primitive);
+			writeByte(outputStream, (Byte)primitive);
 
 		else if(primitive instanceof Double)
-			StreamUtil.writeDouble(outputStream, (Double)primitive);
+			writeDouble(outputStream, (Double)primitive);
 
 		else if(primitive instanceof Character)
-			StreamUtil.writeChar(outputStream, (Character)primitive);
+			writeChar(outputStream, (Character)primitive);
 
 		else if(primitive instanceof Boolean)
-			StreamUtil.writeBoolean(outputStream, (Boolean)primitive);
+			writeBoolean(outputStream, (Boolean)primitive);
 
 		else
 			throw new IllegalArgumentException("Specified type isn't primitive: " + primitive);
@@ -194,25 +214,25 @@ public class SerializationUtil {
 	 * @return number read from stream
 	 */
 	public static Number readNumber(InputStream stream) throws IOException {
-		int type = StreamUtil.readByte(stream);
+		int type = readByte(stream);
 		switch(type) {
 			case INT_T:
-				return StreamUtil.readInt(stream);
+				return readInt(stream);
 
 			case FLOAT_T:
-				return StreamUtil.readFloat(stream);
+				return readFloat(stream);
 
 			case LONG_T:
-				return StreamUtil.readLong(stream);
+				return readLong(stream);
 
 			case DOUBLE_T:
-				return StreamUtil.readDouble(stream);
+				return readDouble(stream);
 
 			case SHORT_T:
-				return StreamUtil.readShort(stream);
+				return readShort(stream);
 
 			case BYTE_T:
-				return StreamUtil.readByte(stream);
+				return readByte(stream);
 
 			case NULL_T:
 				return null;
@@ -229,67 +249,67 @@ public class SerializationUtil {
 	 */
 	public static void writeNumber(OutputStream stream, Number number) throws IOException {
 		if(number == null) {
-			StreamUtil.writeByte(stream, NULL_T);
+			writeByte(stream, NULL_T);
 			return;
 		}
 
 		if(number.getClass() == Integer.class) {
-			StreamUtil.writeByte(stream, INT_T);
-			StreamUtil.writeInt(stream, number.intValue());
+			writeByte(stream, INT_T);
+			writeInt(stream, number.intValue());
 		} else if(number.getClass() == Float.class) {
-			StreamUtil.writeByte(stream, FLOAT_T);
-			StreamUtil.writeFloat(stream, number.floatValue());
+			writeByte(stream, FLOAT_T);
+			writeFloat(stream, number.floatValue());
 		} else if(number.getClass() == Long.class) {
-			StreamUtil.writeByte(stream, LONG_T);
-			StreamUtil.writeLong(stream, number.longValue());
+			writeByte(stream, LONG_T);
+			writeLong(stream, number.longValue());
 		} else if(number.getClass() == Double.class) {
-			StreamUtil.writeByte(stream, DOUBLE_T);
-			StreamUtil.writeDouble(stream, number.doubleValue());
+			writeByte(stream, DOUBLE_T);
+			writeDouble(stream, number.doubleValue());
 		} else if(number.getClass() == Short.class) {
-			StreamUtil.writeByte(stream, SHORT_T);
-			StreamUtil.writeShort(stream, number.shortValue());
+			writeByte(stream, SHORT_T);
+			writeShort(stream, number.shortValue());
 		} else if(number.getClass() == Byte.class) {
-			StreamUtil.writeByte(stream, BYTE_T);
-			StreamUtil.writeByte(stream, number.byteValue());
+			writeByte(stream, BYTE_T);
+			writeByte(stream, number.byteValue());
 		} else
 			throw new IllegalArgumentException("Unknown number type: " + number.getClass());
 	}
 
 	@SuppressWarnings({"unchecked", "RedundantCast"})
 	public static <T> T readAny(InputStream stream, Class<T> type) throws IOException {
-		if(Serializable.class.isAssignableFrom(type))
+		if(Readable.class.isAssignableFrom(type))
 			// this cast is not redundant, this is an error from IntelliJ
-			return (T)readSerializable(stream, type.asSubclass(Serializable.class));
+			return (T)readSerializable(stream, type.asSubclass(Readable.class));
 		else if(type == Color.class)
-			return (T)new Color(StreamUtil.readInt(stream));
+			return (T)new Color(readInt(stream));
 		else if(type == String.class)
 			return (T)StreamUtil.readUTF(stream);
 		else if(type == Vector2.class)
-			return (T)new Vector2(StreamUtil.readFloat(stream), StreamUtil.readFloat(stream));
+			return (T)new Vector2(readFloat(stream), readFloat(stream));
 		else if(type == Vector3.class)
-			return (T)new Vector3(StreamUtil.readFloat(stream), StreamUtil.readFloat(stream), StreamUtil.readFloat(stream));
+			return (T)new Vector3(readFloat(stream), readFloat(stream), readFloat(stream));
 		else if(type.isEnum())
-			return (T)StreamUtil.readEnum(stream, type.asSubclass(Enum.class));
+			return (T)readEnum(stream, type.asSubclass(Enum.class));
 		else
 			return readPrimitive(stream, type);
 	}
 
 	public static void writeAny(OutputStream stream, Object thing) throws IOException {
-		if(thing instanceof Serializable)
-			((Serializable)thing).writeTo(stream);
+		if(thing instanceof Writable)
+			((Writable)thing).writeTo(stream);
 		else if(thing instanceof Color)
-			StreamUtil.writeInt(stream, rgba8888((Color)thing));
+			writeInt(stream, rgba8888((Color)thing));
 		else if(thing instanceof String)
-			StreamUtil.writeUTF(stream, (String)thing);
+			writeUTF(stream, (String)thing);
 		else if(thing instanceof Vector2) {
-			StreamUtil.writeFloat(stream, ((Vector2)thing).x);
-			StreamUtil.writeFloat(stream, ((Vector2)thing).y);
+			writeFloat(stream, ((Vector2)thing).x);
+			writeFloat(stream, ((Vector2)thing).y);
 		} else if(thing instanceof Vector3) {
-			StreamUtil.writeFloat(stream, ((Vector3)thing).x);
-			StreamUtil.writeFloat(stream, ((Vector3)thing).y);
-			StreamUtil.writeFloat(stream, ((Vector3)thing).z);
+			writeFloat(stream, ((Vector3)thing).x);
+			writeFloat(stream, ((Vector3)thing).y);
+			writeFloat(stream, ((Vector3)thing).z);
 		} else if(thing instanceof Enum)
-			StreamUtil.writeEnum(stream, (Enum<?>)thing);
+			writeEnum(stream, (Enum<?>)thing);
 		else
 			writePrimitive(stream, thing);
 	}
