9fb4e782bcd4ebd446cb437798eb053df79cf621
Alexander Winter <a.w1nter@hotmail.com>
2024-03-03 22:59:02 -0500

    API addition: distanceSquaredToAABB

--- src/main/java/com/winteralexander/gdx/utils/math/MathUtil.java
+++ src/main/java/com/winteralexander/gdx/utils/math/MathUtil.java
@@ -136,35 +136,32 @@ public class MathUtil {
 		float lineMaxX = max(lineX1, lineX2);
 		float lineMaxY = max(lineY1, lineY2);
 
-		if(inAABB(lineX1, lineY1, rectX, rectY, width, height) || inAABB(lineX2, lineY2, rectX, rectY, width, height))
+		if(inAABB(lineX1, lineY1, rectX, rectY, width, height)
+		|| inAABB(lineX2, lineY2, rectX, rectY, width, height))
 			return true;
 
-		if(lineMinX < rectX1 && lineMaxX > rectX1) // if it crosses the left limit
-		{
+		if(lineMinX < rectX1 && lineMaxX > rectX1) { // if it crosses the left limit
 			float lineYAtCollision = lineY1 + (lineY2 - lineY1) * (rectX1 - lineX1) / (lineX2 - lineX1);
 
 			if(lineYAtCollision > rectY1 && lineYAtCollision < rectY2)
 				return true;
 		}
 
-		if(lineMinX < rectX2 && lineMaxX > rectX2) // if it crosses the right limit
-		{
+		if(lineMinX < rectX2 && lineMaxX > rectX2) { // if it crosses the right limit
 			float lineYAtCollision = lineY1 + (lineY2 - lineY1) * (rectX2 - lineX1) / (lineX2 - lineX1);
 
 			if(lineYAtCollision > rectY1 && lineYAtCollision < rectY2)
 				return true;
 		}
 
-		if(lineMinY < rectY1 && lineMaxY > rectY1) // if it crosses the bottom limit
-		{
+		if(lineMinY < rectY1 && lineMaxY > rectY1) { // if it crosses the bottom limit
 			float lineXAtCollision = lineX1 + (lineX2 - lineX1) * (rectY1 - lineY1) / (lineY2 - lineY1);
 
 			if(lineXAtCollision > rectX1 && lineXAtCollision < rectX2)
 				return true;
 		}
 
-		if(lineMinY < rectY2 && lineMaxY > rectY2) // if it crosses the top limit
-		{
+		if(lineMinY < rectY2 && lineMaxY > rectY2) { // if it crosses the top limit
 			float lineXAtCollision = lineX1 + (lineX2 - lineX1) * (rectY2 - lineY1) / (lineY2 - lineY1);
 
 			return lineXAtCollision > rectX1 && lineXAtCollision < rectX2;
@@ -173,11 +170,15 @@ public class MathUtil {
 		return false;
 	}
 
-	public static boolean inAABB(float px, float py, float x, float y, float size) {
+	public static boolean inAABB(float px, float py,
+	                             float x, float y,
+	                             float size) {
 		return inAABB(px, py, x, y, size, size);
 	}
 
-	public static boolean inAABB(float px, float py, float x, float y, float width, float height) {
+	public static boolean inAABB(float px, float py,
+	                             float x, float y,
+	                             float width, float height) {
 		if(px > x + width)
 			return false;
 
@@ -190,15 +191,71 @@ public class MathUtil {
 		return py >= y;
 	}
 
-	public static boolean inAABB(Vector2 point, Vector2 startPos, Vector2 size) {
-		return inAABB(point.x, point.y, startPos.x, startPos.y, size.x, size.y);
+	public static boolean inAABB(Vector2 point,
+	                             Vector2 startPos,
+	                             Vector2 size) {
+		return inAABB(point.x, point.y,
+				startPos.x, startPos.y,
+				size.x, size.y);
 	}
 
-	public static boolean inAABB(Vector2 point, Vector2 startPos, Vector2 size, Vector2 origin) {
-		return inAABB(point.x, point.y, startPos.x + origin.x, startPos.y + origin.y, size.x, size.y);
+	public static boolean inAABB(Vector2 point,
+	                             Vector2 startPos,
+	                             Vector2 size,
+	                             Vector2 origin) {
+		return inAABB(point.x, point.y,
+				startPos.x + origin.x, startPos.y + origin.y,
+				size.x, size.y);
 	}
 
-	public static boolean doAABBCollideWithAABB(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
+	public static float distanceSquaredToAABB(float px, float py,
+	                                          float x, float y,
+	                                          float size) {
+		return distanceSquaredToAABB(px, py, x, y, size, size);
+	}
+
+	public static float distanceSquaredToAABB(float px, float py,
+	                                          float x, float y,
+	                                          float width, float height) {
+		float closestX = px;
+		float closestY = py;
+
+		if(px > x + width)
+			closestX = x + width;
+
+		if(px < x)
+			closestX = x;
+
+		if(py > y + height)
+			closestY = y + height;
+
+		if(py < y)
+			closestY = y;
+
+		return pow2(x - closestX) + pow2(y - closestY);
+	}
+
+	public static float distanceSquaredToAABB(Vector2 point,
+	                                          Vector2 startPos,
+	                                          Vector2 size) {
+		return distanceSquaredToAABB(point.x, point.y,
+				startPos.x, startPos.y,
+				size.x, size.y);
+	}
+
+	public static float distanceSquaredToAABB(Vector2 point,
+	                                          Vector2 startPos,
+	                                          Vector2 size,
+	                                          Vector2 origin) {
+		return distanceSquaredToAABB(point.x, point.y,
+				startPos.x + origin.x, startPos.y + origin.y,
+				size.x, size.y);
+	}
+
+	public static boolean doAABBCollideWithAABB(float x1, float y1,
+	                                            float w1, float h1,
+	                                            float x2, float y2,
+	                                            float w2, float h2) {
 		if(x1 + w1 <= x2)
 			return false;
 
@@ -224,7 +281,10 @@ public class MathUtil {
 	 * @param h2 height of box 2
 	 * @return true if box 1 is fully inside box 2, otherwise false
 	 */
-	public static boolean isAABBfullyInAABB(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
+	public static boolean isAABBfullyInAABB(float x1, float y1,
+	                                        float w1, float h1,
+	                                        float x2, float y2,
+	                                        float w2, float h2) {
 		return x1 + w1 <= x2 + w2 && x1 >= x2 && y1 + h1 <= y2 + h2 && y1 >= y2;
 	}
 
