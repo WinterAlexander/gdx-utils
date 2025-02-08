f56b54769de84c9f2731d5b848f672ba54070b61
Alexander Winter <a.w1nter@hotmail.com>
2025-02-07 22:35:51 -0500

    Added IcoSphereShapeBuilder

---
+++ src/main/java/com/winteralexander/gdx/utils/g3d/IcoSphereShapeBuilder.java
@@ -0,0 +1,134 @@
+package com.winteralexander.gdx.utils.g3d;
+
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.BaseShapeBuilder;
+import com.badlogic.gdx.utils.FloatArray;
+import com.badlogic.gdx.utils.ShortArray;
+
+/**
+ * Shape builder for IcoSpheres
+ * <p>
+ * Created on 2025-02-07.
+ *
+ * @author Alexander Winter
+ */
+public class IcoSphereShapeBuilder extends BaseShapeBuilder {
+	private static final FloatArray tmpVertices = new FloatArray();
+	private static final ShortArray tmpIndices = new ShortArray();
+	private static final ShortArray tmpNewIndices = new ShortArray();
+
+	public static void build(MeshPartBuilder builder, float radius, int subdivisions) {
+		tmpVertices.clear();
+		tmpIndices.clear();
+
+		// Create the initial icosahedron vertices and indices
+		createIcosahedron(tmpVertices, tmpIndices);
+
+		// Subdivide the icosahedron
+		for(int i = 0; i < subdivisions; i++)
+			subdivide(tmpVertices, tmpIndices);
+
+		// Normalize vertices to create a sphere
+		for(int i = 0; i < tmpVertices.size; i += 3) {
+			vertTmp0.setPos(tmpVertices.get(i), tmpVertices.get(i + 1), tmpVertices.get(i + 2));
+			vertTmp0.position.nor();
+			vertTmp0.setNor(vertTmp0.position);
+			vertTmp0.position.scl(radius);
+			vertTmp0.setUV(0f, 0f);
+			builder.vertex(vertTmp0);
+		}
+
+		for(int i = 0; i < tmpIndices.size; i += 3)
+			builder.triangle(tmpIndices.get(i), tmpIndices.get(i + 1), tmpIndices.get(i + 2));
+		tmpVertices.clear();
+		tmpIndices.clear();
+	}
+
+	private static void createIcosahedron(FloatArray vertices, ShortArray indices) {
+		// Create the 12 vertices of the icosahedron
+		float t = (1.0f + (float) Math.sqrt(5.0f)) / 2.0f;
+
+		tmpV0.set(-1f, t, 0);
+		tmpV1.set(1f, t, 0);
+		tmpV2.set(-1f, -t, 0);
+		tmpV3.set(1f, -t, 0);
+
+		vertices.ensureCapacity(36);
+		vertices.add(tmpV0.x, tmpV0.y, tmpV0.z);
+		vertices.add(tmpV1.x, tmpV1.y, tmpV1.z);
+		vertices.add(tmpV2.x, tmpV2.y, tmpV2.z);
+		vertices.add(tmpV3.x, tmpV3.y, tmpV3.z);
+
+		vertices.add(tmpV0.z, tmpV0.x, tmpV0.y);
+		vertices.add(tmpV1.z, tmpV1.x, tmpV1.y);
+		vertices.add(tmpV2.z, tmpV2.x, tmpV2.y);
+		vertices.add(tmpV3.z, tmpV3.x, tmpV3.y);
+
+		vertices.add(tmpV0.y, tmpV0.z, tmpV0.x);
+		vertices.add(tmpV1.y, tmpV1.z, tmpV1.x);
+		vertices.add(tmpV2.y, tmpV2.z, tmpV2.x);
+		vertices.add(tmpV3.y, tmpV3.z, tmpV3.x);
+
+		// Create the 20 faces of the icosahedron
+		indices.ensureCapacity(60);
+		indices.add((short)0, (short)11, (short)5);
+		indices.add((short)0, (short)5, (short)1);
+		indices.add((short)0, (short)1, (short)7);
+		indices.add((short)0, (short)7, (short)10);
+		indices.add((short)0, (short)10, (short)11);
+
+		indices.add((short)1, (short)5, (short)9);
+		indices.add((short)5, (short)11, (short)4);
+		indices.add((short)11, (short)10, (short)2);
+		indices.add((short)10, (short)7, (short)6);
+		indices.add((short)7, (short)1, (short)8);
+
+		indices.add((short)3, (short)9, (short)4);
+		indices.add((short)3, (short)4, (short)2);
+		indices.add((short)3, (short)2, (short)6);
+		indices.add((short)3, (short)6, (short)8);
+		indices.add((short)3, (short)8, (short)9);
+
+		indices.add((short)4, (short)9, (short)5);
+		indices.add((short)2, (short)4, (short)11);
+		indices.add((short)6, (short)2, (short)10);
+		indices.add((short)8, (short)6, (short)7);
+		indices.add((short)9, (short)8, (short)1);
+	}
+
+	private static void subdivide(FloatArray vertices, ShortArray indices) {
+		// Subdivide each triangle into 4 smaller triangles
+		tmpNewIndices.clear();
+		int numTriangles = indices.size / 3;
+
+		for (int i = 0; i < numTriangles; i++) {
+			int i1 = indices.get(i * 3);
+			int i2 = indices.get(i * 3 + 1);
+			int i3 = indices.get(i * 3 + 2);
+
+			tmpV0.set(vertices.get(i1 * 3), vertices.get(i1 * 3 + 1), vertices.get(i1 * 3 + 2));
+			tmpV1.set(vertices.get(i2 * 3), vertices.get(i2 * 3 + 1), vertices.get(i2 * 3 + 2));
+			tmpV2.set(vertices.get(i3 * 3), vertices.get(i3 * 3 + 1), vertices.get(i3 * 3 + 2));
+
+			tmpV3.set(tmpV0).add(tmpV1).scl(0.5f);
+			tmpV4.set(tmpV1).add(tmpV2).scl(0.5f);
+			tmpV5.set(tmpV2).add(tmpV0).scl(0.5f);
+
+			int i12 = vertices.size / 3;
+			vertices.add(tmpV3.x, tmpV3.y, tmpV3.z);
+			int i23 = vertices.size / 3;
+			vertices.add(tmpV4.x, tmpV4.y, tmpV4.z);
+			int i31 = vertices.size / 3;
+			vertices.add(tmpV5.x, tmpV5.y, tmpV5.z);
+
+			tmpNewIndices.addAll((short)i1, (short)i12, (short)i31);
+			tmpNewIndices.addAll((short)i12, (short)i2, (short)i23);
+			tmpNewIndices.addAll((short)i31, (short)i23, (short)i3);
+			tmpNewIndices.addAll((short)i12, (short)i23, (short)i31);
+		}
+
+		indices.clear();
+		indices.addAll(tmpNewIndices);
+		tmpNewIndices.clear();
+	}
+}
