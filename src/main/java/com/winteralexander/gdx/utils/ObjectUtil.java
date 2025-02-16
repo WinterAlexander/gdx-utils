8587f48b40a9fbdb13f5bb1f3d8dc403be005874
Alexander Winter <a.w1nter@hotmail.com>
2025-02-16 10:59:44 -0500

    ObjectUtil#firstNonNegative extra overloads

--- src/main/java/com/winteralexander/gdx/utils/ObjectUtil.java
+++ src/main/java/com/winteralexander/gdx/utils/ObjectUtil.java
@@ -60,17 +60,71 @@ public class ObjectUtil {
 	}
 
 	/**
-	 * Returns the first non-negative object of the specified parameters, in order
+	 * Returns the first non-negative integer of the specified parameters, in order
 	 *
 	 * @param i1 integer 1
 	 * @param i2 integer 2
 	 * @param i3 integer 3
-	 * @return first non-negative object of parameters or -1 if none non-negative
+	 * @return first non-negative integer of parameters or -1 if none non-negative
 	 */
 	public static int firstNonNegative(int i1, int i2, int i3) {
 		return i1 > 0 ? i1 :
 				i2 > 0 ? i2 :
-						i3 > 0 ? i3 : -1;
+				i3 > 0 ? i3 : -1;
+	}
+
+	/**
+	 * Returns the first non-negative integer of the specified parameters, in order
+	 *
+	 * @param i1 integer 1
+	 * @param i2 integer 2
+	 * @param i3 integer 3
+	 * @param i4 integer 4
+	 * @return first non-negative integer of parameters or -1 if none non-negative
+	 */
+	public static int firstNonNegative(int i1, int i2, int i3, int i4) {
+		return i1 > 0 ? i1 :
+				i2 > 0 ? i2 :
+				i3 > 0 ? i3 :
+				i4 > 0 ? i4 : -1;
+	}
+
+	/**
+	 * Returns the first non-negative integer of the specified parameters, in order
+	 *
+	 * @param i1 integer 1
+	 * @param i2 integer 2
+	 * @param i3 integer 3
+	 * @param i4 integer 4
+	 * @param i5 integer 5
+	 * @return first non-negative integer of parameters or -1 if none non-negative
+	 */
+	public static long firstNonNegative(int i1, int i2, int i3, int i4, int i5) {
+		return i1 > 0 ? i1 :
+				i2 > 0 ? i2 :
+				i3 > 0 ? i3 :
+				i4 > 0 ? i4 :
+				i5 > 0 ? i5 : -1;
+	}
+
+	/**
+	 * Returns the first non-negative integer of the specified parameters, in order
+	 *
+	 * @param i1 integer 1
+	 * @param i2 integer 2
+	 * @param i3 integer 3
+	 * @param i4 integer 4
+	 * @param i5 integer 5
+	 * @param i6 integer 6
+	 * @return first non-negative integer of parameters or -1 if none non-negative
+	 */
+	public static long firstNonNegative(int i1, int i2, int i3, int i4, int i5, int i6) {
+		return i1 > 0 ? i1 :
+				i2 > 0 ? i2 :
+				i3 > 0 ? i3 :
+				i4 > 0 ? i4 :
+				i5 > 0 ? i5 :
+				i6 > 0 ? i6 : -1;
 	}
 
 	/**
@@ -100,17 +154,71 @@ public class ObjectUtil {
 	}
 
 	/**
-	 * Returns the first non-negative object of the specified parameters, in order
+	 * Returns the first non-negative integer of the specified parameters, in order
 	 *
 	 * @param i1 long integer 1
 	 * @param i2 long integer 2
 	 * @param i3 long integer 3
-	 * @return first non-negative object of parameters or -1 if none non-negative
+	 * @return first non-negative integer of parameters or -1 if none non-negative
 	 */
 	public static long firstNonNegative(long i1, long i2, long i3) {
 		return i1 > 0 ? i1 :
 				i2 > 0 ? i2 :
-						i3 > 0 ? i3 : -1;
+				i3 > 0 ? i3 : -1;
+	}
+
+	/**
+	 * Returns the first non-negative integer of the specified parameters, in order
+	 *
+	 * @param i1 long integer 1
+	 * @param i2 long integer 2
+	 * @param i3 long integer 3
+	 * @param i4 long integer 4
+	 * @return first non-negative integer of parameters or -1 if none non-negative
+	 */
+	public static long firstNonNegative(long i1, long i2, long i3, long i4) {
+		return i1 > 0 ? i1 :
+				i2 > 0 ? i2 :
+				i3 > 0 ? i3 :
+				i4 > 0 ? i4 : -1;
+	}
+
+	/**
+	 * Returns the first non-negative integer of the specified parameters, in order
+	 *
+	 * @param i1 long integer 1
+	 * @param i2 long integer 2
+	 * @param i3 long integer 3
+	 * @param i4 long integer 4
+	 * @param i5 long integer 5
+	 * @return first non-negative integer of parameters or -1 if none non-negative
+	 */
+	public static long firstNonNegative(long i1, long i2, long i3, long i4, long i5) {
+		return i1 > 0 ? i1 :
+				i2 > 0 ? i2 :
+				i3 > 0 ? i3 :
+				i4 > 0 ? i4 :
+				i5 > 0 ? i5 : -1;
+	}
+
+	/**
+	 * Returns the first non-negative integer of the specified parameters, in order
+	 *
+	 * @param i1 long integer 1
+	 * @param i2 long integer 2
+	 * @param i3 long integer 3
+	 * @param i4 long integer 4
+	 * @param i5 long integer 5
+	 * @param i6 long integer 6
+	 * @return first non-negative integer of parameters or -1 if none non-negative
+	 */
+	public static long firstNonNegative(long i1, long i2, long i3, long i4, long i5, long i6) {
+		return i1 > 0 ? i1 :
+				i2 > 0 ? i2 :
+				i3 > 0 ? i3 :
+				i4 > 0 ? i4 :
+				i5 > 0 ? i5 :
+				i6 > 0 ? i6 : -1;
 	}
 
 	/**
