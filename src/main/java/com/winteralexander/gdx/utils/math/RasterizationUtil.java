56efdbde725c78c56d82d35eae5f10ba088b2d8f
Alexander Winter <a.w1nter@hotmail.com>
2025-03-02 00:47:58 -0500

    API Addition : Generic 2d line Rasterizer

---
+++ src/main/java/com/winteralexander/gdx/utils/math/RasterizationUtil.java
@@ -0,0 +1,84 @@
+package com.winteralexander.gdx.utils.math;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.winteralexander.gdx.utils.math.vector.Vector2i;
+
+import java.util.function.Consumer;
+
+import static java.lang.Math.abs;
+
+/**
+ * Utility class to perform rasterization
+ * <p>
+ * Created on 2025-03-01.
+ *
+ * @author Alexander Winter
+ */
+public class RasterizationUtil {
+	public static void rasterizeLine2D(float startX, float startY,
+	                                   float endX, float endY,
+	                                   Array<Vector2i> tilesOut) {
+		rasterizeLine2D(startX, startY, endX, endY, vector2i -> tilesOut.add(vector2i.cpy()));
+	}
+
+	public static void rasterizeLine2D(float startX, float startY,
+	                                   float endX, float endY,
+	                                   Consumer<Vector2i> tileConsumer) {
+		rasterizeLine2D(startX, startY, endX, endY, tileConsumer, new Vector2i());
+	}
+
+	public static void rasterizeLine2D(float startX, float startY,
+	                                   float endX, float endY,
+	                                   Consumer<Vector2i> tileConsumer,
+	                                   Vector2i tmpTile) {
+		float dx = abs(endX - startX);
+		float dy = -abs(endY - startY);
+		float error = dx + dy;
+
+		int x0 = MathUtils.floor(startX);
+		int y0 = MathUtils.floor(startY);
+
+		int x1 = MathUtils.ceil(endX);
+		int y1 = MathUtils.ceil(endY);
+
+		int sx = x0 < x1 ? 1 : -1;
+		int sy = y0 < y1 ? 1 : -1;
+
+		while(true) {
+			tileConsumer.accept(tmpTile.set(x0, y0));
+			if(error * 2f >= dy) {
+				if(x0 == x1)
+					break;
+				error += dy;
+				x0 += sx;
+			}
+			if(error * 2f <= dx) {
+				if(y0 == y1)
+					break;
+				error += dx;
+				y0 += sy;
+			}
+		}
+	}
+
+	public static void rasterizeLine2D(Vector2 start,
+	                                   Vector2 end,
+	                                   Array<Vector2i> tilesOut) {
+		rasterizeLine2D(start.x, start.y, end.x, end.y, tilesOut);
+	}
+
+	public static void rasterizeLine2D(Vector2 start,
+	                                   Vector2 end,
+	                                   Consumer<Vector2i> tileConsumer) {
+		rasterizeLine2D(start.x, start.y, end.x, end.y, tileConsumer, new Vector2i());
+	}
+
+	public static void rasterizeLine2D(Vector2 start,
+	                                   Vector2 end,
+	                                   Consumer<Vector2i> tileConsumer,
+	                                   Vector2i tmpTile) {
+		rasterizeLine2D(start.x, start.y, end.x, end.y, tileConsumer, tmpTile);
+	}
+}
---
+++ src/test/java/com/winteralexander/gdx/utils/test/math/RasterizationUtilTest.java
@@ -0,0 +1,48 @@
+package com.winteralexander.gdx.utils.test.math;
+
+import com.badlogic.gdx.utils.Array;
+import com.winteralexander.gdx.utils.ReflectionUtil;
+import com.winteralexander.gdx.utils.math.RasterizationUtil;
+import com.winteralexander.gdx.utils.math.vector.Vector2i;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link RasterizationUtil}
+ * <p>
+ * Created on 2025-03-02.
+ *
+ * @author Alexander Winter
+ */
+public class RasterizationUtilTest {
+	@Test
+	public void testLineRasterization2D() {
+		Array<Vector2i> out = new Array<>();
+		RasterizationUtil.rasterizeLine2D(0.111f, 0.3123f, 100.123f, 35.213f, out);
+		//System.out.println(ReflectionUtil.toPrettyString(out));
+		printRaster(out);
+	}
+
+	private void printRaster(Array<Vector2i> tiles) {
+		int minX = tiles.get(0).x;
+		int minY = tiles.get(0).y;
+		int maxX = tiles.get(0).x;
+		int maxY = tiles.get(0).y;
+
+		for(Vector2i tile : tiles) {
+			minX = Math.min(minX, tile.x);
+			minY = Math.min(minY, tile.y);
+			maxX = Math.max(maxX, tile.x);
+			maxY = Math.max(maxY, tile.y);
+		}
+
+		for(int j = minY; j <= maxY; j++) {
+			for(int i = minX; i <= maxX; i++) {
+				if(tiles.contains(new Vector2i(i, j), false))
+					System.out.print('X');
+				else
+					System.out.print('_');
+			}
+			System.out.print('\n');
+		}
+	}
+}
