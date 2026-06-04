6af80c88870d9ffb469012095933d935a68e87fe
Alexander Winter <a.w1nter@hotmail.com>
2026-06-04 01:23:21 -0400

    Refactor static variable names (not constants so not upper case)

--- src/main/java/com/winteralexander/gdx/utils/gfx/GLUtil.java
+++ src/main/java/com/winteralexander/gdx/utils/gfx/GLUtil.java
@@ -20,90 +20,90 @@ public class GLUtil {
 	public static final int GL_LINE_SMOOTH = 2_848;
 	public static final int GL_SMOOTH_LINE_WIDTH_RANGE = 0xB22;
 
-	private static int MAX_TEXTURE_IMAGE_UNITS = -1;
-	private static int MAX_TEXTURE_SIZE = -1;
-	private static int MAX_VERTEX_UNIFORM_VECTORS = -1;
+	private static int maxTextureImageUnits = -1;
+	private static int maxTextureSize = -1;
+	private static int maxVertexUniformVectors = -1;
 
-	private static int MIN_SMOOTH_LINE_WIDTH = -1;
-	private static int MAX_SMOOTH_LINE_WIDTH = -1;
-	private static int MIN_ALIASED_LINE_WIDTH = -1;
-	private static int MAX_ALIASED_LINE_WIDTH = -1;
+	private static int minSmoothLineWidth = -1;
+	private static int maxSmoothLineWidth = -1;
+	private static int minAliasedLineWidth = -1;
+	private static int maxAliasedLineWidth = -1;
 
 	public static int getMaxTextureImageUnits() {
-		if(MAX_TEXTURE_IMAGE_UNITS != -1)
-			return MAX_TEXTURE_IMAGE_UNITS;
+		if(maxTextureImageUnits != -1)
+			return maxTextureImageUnits;
 
 		IntBuffer
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, buffer);
-		return MAX_TEXTURE_IMAGE_UNITS = buffer.get();
+		return maxTextureImageUnits = buffer.get();
 	}
 
 	public static int getMaxTextureSize() {
-		if(MAX_TEXTURE_SIZE != -1)
-			return MAX_TEXTURE_SIZE;
+		if(maxTextureSize != -1)
+			return maxTextureSize;
 
 		IntBuffer
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_MAX_TEXTURE_SIZE, buffer);
-		return MAX_TEXTURE_SIZE = buffer.get();
+		return maxTextureSize = buffer.get();
 	}
 
 	public static int getMaxVertexUniformVectors() {
-		if(MAX_VERTEX_UNIFORM_VECTORS != -1)
-			return MAX_VERTEX_UNIFORM_VECTORS;
+		if(maxVertexUniformVectors != -1)
+			return maxVertexUniformVectors;
 
 		IntBuffer
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_MAX_VERTEX_UNIFORM_VECTORS, buffer);
-		return MAX_VERTEX_UNIFORM_VECTORS = buffer.get();
+		return maxVertexUniformVectors = buffer.get();
 	}
 
 	public static int getMinSmoothLineWidth() {
-		if(MIN_SMOOTH_LINE_WIDTH != -1)
-			return MIN_SMOOTH_LINE_WIDTH;
+		if(minSmoothLineWidth != -1)
+			return minSmoothLineWidth;
 
 		IntBuffer
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_SMOOTH_LINE_WIDTH_RANGE, buffer);
-		MIN_SMOOTH_LINE_WIDTH = buffer.get();
-		MAX_SMOOTH_LINE_WIDTH = buffer.get();
-		return MIN_SMOOTH_LINE_WIDTH;
+		minSmoothLineWidth = buffer.get();
+		maxSmoothLineWidth = buffer.get();
+		return minSmoothLineWidth;
 	}
 
 	public static int getMaxSmoothLineWidth() {
-		if(MAX_SMOOTH_LINE_WIDTH != -1)
-			return MAX_SMOOTH_LINE_WIDTH;
+		if(maxSmoothLineWidth != -1)
+			return maxSmoothLineWidth;
 
 		IntBuffer
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_SMOOTH_LINE_WIDTH_RANGE, buffer);
-		MIN_SMOOTH_LINE_WIDTH = buffer.get();
-		MAX_SMOOTH_LINE_WIDTH = buffer.get();
-		return MAX_SMOOTH_LINE_WIDTH;
+		minSmoothLineWidth = buffer.get();
+		maxSmoothLineWidth = buffer.get();
+		return maxSmoothLineWidth;
 	}
 
 	public static int getMinAliasLineWidth() {
-		if(MIN_ALIASED_LINE_WIDTH != -1)
-			return MIN_ALIASED_LINE_WIDTH;
+		if(minAliasedLineWidth != -1)
+			return minAliasedLineWidth;
 
 		IntBuffer
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_ALIASED_LINE_WIDTH_RANGE, buffer);
-		MIN_ALIASED_LINE_WIDTH = buffer.get();
-		MAX_ALIASED_LINE_WIDTH = buffer.get();
-		return MIN_ALIASED_LINE_WIDTH;
+		minAliasedLineWidth = buffer.get();
+		maxAliasedLineWidth = buffer.get();
+		return minAliasedLineWidth;
 	}
 
 	public static int getMaxAliasLineWidth() {
-		if(MAX_ALIASED_LINE_WIDTH != -1)
-			return MAX_ALIASED_LINE_WIDTH;
+		if(maxAliasedLineWidth != -1)
+			return maxAliasedLineWidth;
 
 		IntBuffer
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_ALIASED_LINE_WIDTH_RANGE, buffer);
-		MIN_ALIASED_LINE_WIDTH = buffer.get();
-		MAX_ALIASED_LINE_WIDTH = buffer.get();
-		return MAX_ALIASED_LINE_WIDTH;
+		minAliasedLineWidth = buffer.get();
+		maxAliasedLineWidth = buffer.get();
+		return maxAliasedLineWidth;
 	}
 }
