066f7a64dfa8da818628a22358b88ab564f9d024
Alexander Winter <a.w1nter@hotmail.com>
2026-06-04 01:02:31 -0400

    Added utility methods for retrieving line max widths

--- src/main/java/com/winteralexander/gdx/utils/gfx/GLUtil.java
+++ src/main/java/com/winteralexander/gdx/utils/gfx/GLUtil.java
@@ -17,9 +17,17 @@ import static com.badlogic.gdx.graphics.GL20.*;
  * @author Alexander Winter
  */
 public class GLUtil {
+	public static final int GL_LINE_SMOOTH = 2_848;
+	public static final int GL_SMOOTH_LINE_WIDTH_RANGE = 0xB22;
+
 	private static int MAX_TEXTURE_IMAGE_UNITS = -1;
 	private static int MAX_TEXTURE_SIZE = -1;
 	private static int MAX_VERTEX_UNIFORM_VECTORS = -1;
+
+	private static int MIN_SMOOTH_LINE_WIDTH = -1;
+	private static int MAX_SMOOTH_LINE_WIDTH = -1;
+	private static int MIN_ALIASED_LINE_WIDTH = -1;
+	private static int MAX_ALIASED_LINE_WIDTH = -1;
 
 	public static int getMaxTextureImageUnits() {
 		if(MAX_TEXTURE_IMAGE_UNITS != -1)
@@ -49,5 +57,53 @@ public class GLUtil {
 				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
 		Gdx.gl.glGetIntegerv(GL_MAX_VERTEX_UNIFORM_VECTORS, buffer);
 		return MAX_VERTEX_UNIFORM_VECTORS = buffer.get();
+	}
+
+	public static int getMinSmoothLineWidth() {
+		if(MIN_SMOOTH_LINE_WIDTH != -1)
+			return MIN_SMOOTH_LINE_WIDTH;
+
+		IntBuffer
+				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
+		Gdx.gl.glGetIntegerv(GL_SMOOTH_LINE_WIDTH_RANGE, buffer);
+		MIN_SMOOTH_LINE_WIDTH = buffer.get();
+		MAX_SMOOTH_LINE_WIDTH = buffer.get();
+		return MIN_SMOOTH_LINE_WIDTH;
+	}
+
+	public static int getMaxSmoothLineWidth() {
+		if(MAX_SMOOTH_LINE_WIDTH != -1)
+			return MAX_SMOOTH_LINE_WIDTH;
+
+		IntBuffer
+				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
+		Gdx.gl.glGetIntegerv(GL_SMOOTH_LINE_WIDTH_RANGE, buffer);
+		MIN_SMOOTH_LINE_WIDTH = buffer.get();
+		MAX_SMOOTH_LINE_WIDTH = buffer.get();
+		return MAX_SMOOTH_LINE_WIDTH;
+	}
+
+	public static int getMinAliasLineWidth() {
+		if(MIN_ALIASED_LINE_WIDTH != -1)
+			return MIN_ALIASED_LINE_WIDTH;
+
+		IntBuffer
+				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
+		Gdx.gl.glGetIntegerv(GL_ALIASED_LINE_WIDTH_RANGE, buffer);
+		MIN_ALIASED_LINE_WIDTH = buffer.get();
+		MAX_ALIASED_LINE_WIDTH = buffer.get();
+		return MIN_ALIASED_LINE_WIDTH;
+	}
+
+	public static int getMaxAliasLineWidth() {
+		if(MAX_ALIASED_LINE_WIDTH != -1)
+			return MAX_ALIASED_LINE_WIDTH;
+
+		IntBuffer
+				buffer = ByteBuffer.allocateDirect(64).order(ByteOrder.nativeOrder()).asIntBuffer();
+		Gdx.gl.glGetIntegerv(GL_ALIASED_LINE_WIDTH_RANGE, buffer);
+		MIN_ALIASED_LINE_WIDTH = buffer.get();
+		MAX_ALIASED_LINE_WIDTH = buffer.get();
+		return MAX_ALIASED_LINE_WIDTH;
 	}
 }
