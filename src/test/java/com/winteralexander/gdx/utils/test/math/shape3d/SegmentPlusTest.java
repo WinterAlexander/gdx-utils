fe1961f4194b9ed507569ab7a79c1c8058ad93e7
Alexander Winter <a.w1nter@hotmail.com>
2026-06-27 10:12:10 -0400

    Applied formatter

--- src/test/java/com/winteralexander/gdx/utils/test/math/shape3d/SegmentPlusTest.java
+++ src/test/java/com/winteralexander/gdx/utils/test/math/shape3d/SegmentPlusTest.java
@@ -17,9 +17,9 @@ public class SegmentPlusTest {
 	@Test
 	public void testGetParameterPrecision() {
 		assertEquals(1f,
-				SegmentPlus.getParameter(
-						new Vector3(-0.1f, 0.01f, -0.5f),
+				SegmentPlus.getParameter(new Vector3(-0.1f, 0.01f, -0.5f),
 						new Vector3(-0.075f, 0.01f, -0.5f),
-						new Vector3(-0.075f, 0.01f, -0.5f)), 0f);
+						new Vector3(-0.075f, 0.01f, -0.5f)),
+				0f);
 	}
 }
